e7b4783b49fcb37d3a12758689f8ea049badeefd
Rauch Rodrigues <94990103+rauchrods@users.noreply.github.com>
2023-03-19 10:59:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 //your code here
 
+function sort(touristSpots){
+	touristSpots.sort((a,b)=>{
+   let a1 = a.replace(/(the |a |an )/i, "");
+   let b1 = b.replace(/(the |a |an )/i, "");
+
+   if (a1 < b1) {
+    return -1;
+  }
+
+  if (a1 > b1) {
+    return 1;
+  }
+
+  return 0;
+   
+});
+
+	return touristSpots;
+}
+
